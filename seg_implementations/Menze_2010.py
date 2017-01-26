a98774c06cebed5665266eb0b48799514505f248
jakubk <jakubk@mit.edu>
2017-01-26 10:26:25 -0500

    Functions return value for one voxel

--- seg_implementations/Menze_2010.py
+++ seg_implementations/Menze_2010.py
@@ -9,21 +9,15 @@ Menze, B. H., Van Leemput, K., Lashkari, D., Weber, M. A., Ayache, N., &
 
 Notes
 -----
-c is number of channels (observations for each voxel). Each imaging modality?
+C is number of channels (observations for each voxel). Imaging modalities?
 k sub i is tissue class of voxel i. This is known.
 a is probabilistic atlas (prob of being tumor).
 a sub i is voxel i's probability of being a tumor. This is unknown.
 t sub i is a binary vector of length c indicating whether voxel i is classified
-    as tumor in each channel c.
+    as tumor in each channel c. Value of 1 indicates tumor is present.
 """
-from __future__ import print_function
-
+from __future__ import division, print_function
 import numpy as np
-
-C = 2  # Number of image modalities.
-K = 3  # Number of tissue classes.
-orig_slice = np.ones((256, 256))
-img_x, img_y = orig_slice.shape  # Image dimensions.
 
 # 1. Generative tumor model
 #
@@ -36,39 +30,26 @@ def normal_state():
     """"""
     pass
 
+
 # 1b. Tumor state -- p(t sub i; a sub i)
 #   The probability that tumor is present in voxel i.
-#   Array `t` could have shape (1, C) or shape (n_voxels, C) (or somethine else)
-#   Better: `t` could have shape (X, Y, C). For example, (256, 256, 4).
-
-
-a = np.zeros((img_x, img_y))  # Tumor presence probabilities. (How is this different from t?)
-t_c = np.zeros((img_x, img_y, C))  # Binary array of tumor presence in each channel.
-t = np.zeros((img_x, img_y))  # Initialize empty array for tumor prob values.
-
-def tumor_state(a, t_c, C):
-    """Return array of voxel-wise probability of tumor presence. A greater value
-    indicates greater chance that tumor is present in that voxel.
+def tumor_state(a_voxel, t_voxel):
+    """Return probability that voxel is a tumor, given "latent" probabilistic
+    atlas. In the paper, this is p(t sub i; a sub i), where i is a voxel.
 
     Parameters
     ----------
-    a: array of tumor presence probabilities.
-    t_c: array of latent tumor state in each channel. Atlas `a` in the paper.
-    C: int, number of imaging modalities.
-
-    Returns
-    -------
-    t: array of voxel-wise probability of tumor presence.
+    a_voxel: scalar
+        Probability that this voxel is a tumor. Defined by "latent"
+        probabilistic atlas.
+    t_voxel: ndarray
+        Array of {0, 1} that indicates presence of tumor in channel c. Value of
+        1 indicates tumor is present. Array has shape (C, ), where C is the
+        number of channels.
     """
-    img_x, img_y = a.shape
-    t = np.zeros((img_x, img_y))
-    # Look at np.nditer docs to iterate over two arrays at once.
-    for x in range(img_x):
-        for y in range(img_y):
-            t[x, y] = np.prod([np.power(a[x, y], t_c[x, y, c])
-                               * np.power((1 - a[x, y]), (1 - t_c[x, y, c]))
-                               for c in range(C)])
-    return t
+    return np.prod([np.power(a_voxel, t_voxel_c)
+                    * np.power((1 - a_voxel), (1 - t_voxel_c))
+                    for t_voxel_c in t_voxel])
 
 
 # 1c. Data likelihood -- p(y sub i | t sub i, k sub i; theta.)
@@ -77,34 +58,42 @@ def tumor_state(a, t_c, C):
 # # Make gaussian distribution.
 # np.random.normal(mean, stdev), where stdev == np.sqrt(variance)
 #
-# theta is the set of all mean and variance parameters for the gaussian distributions.
+# theta is the set of all mean and variance parameters for the gaussian
+# distributions.
 # Should have shape (n_tissue_classes + 1, n_channels, 2).
 # First axis is n_tissue_classes + 1 to account for the tumor.
 #
 # y sub i is vector of intensity observations at voxel i for each channel. Does
 # this mean y sub i has the original intensity values for each channel?
 
-theta = np.zeros((K + 1, C, 2))  # Last axis contains (mean, stdev).
+theta = np.zeros((K + 1, C, 2))
+# This is not correct yet. But this is the basic idea.
+gaussians = np.array([np.random.normal(mean, np.sqrt(var))
+                      for mean, var in theta])
 
-def data_likelihood():
-    y = np.zeros((img_x, img_y))
-    for x in range(img_x):
-        for y in range(img_y):
-            this_k = 0  # Predicted tissue class of this voxel. From normal_state().
-            y[x, y] = np.prod([np.power(dist_1(theta[this_K, c, 0], theta[this_K, c, 1]), (1 - t_c[x, y, c]))
-             * np.power(dist_2(theta[K+1, c, 0], theta[K+1, c, 1]), t_c[x, y, c])
-             for c in range(C)])
+def data_likelihood(y_voxel, k_voxel, t_voxel, gaussians, K):
+    """Return image observation for a voxel. In the paper, this is
+    p(y sub i | t sub i, k sub i; theta)
 
+    Create Gaussians outside of the function, so distributions are not created
+    for each voxel.
+
+    y_voxel: ndarray
+        Array of intensity observations at one voxel in each channel.
+    k_voxel: int
+        The tissue class of the voxel.
+    t_voxel: ndarray
+        Array of {0, 1} that indicates presence of tumor in channel c. Value of
+        1 indicates tumor is present. 1-D array with length C, where C is the
+        number of channels.
+    gaussians: ndarray
+        1-D array of normal distributions with length (K + 1).
+    K: int
+        Number of healthy tissue classes.
+    """
+    return np.prod([np.power(gaussians[k_voxel, c], (1 - t_voxel[c]))
+                    * np.power(gaussians[K + 1, c], t_voxel[c])
+                    for c in range(t_voxel)])
 
 
 # 2. Maximum likelihood parameter estimation
-
-
-
-
-
-
-
-
-
-#
